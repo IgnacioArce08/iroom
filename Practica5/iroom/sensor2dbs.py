f352a8a2236cee55ef37e78e2c02091fc62cb2e7
IgnacioArce08 <ignacio.arce@edu.uah.es>
2023-12-18 20:13:31 +0100

    a

--- Practica5/iroom/sensor2dbs.py
+++ Practica5/iroom/sensor2dbs.py
@@ -14,7 +14,7 @@ type_sensor = ['temperature', 'humidity', 'light', 'sound', 'motion']
 last_value = [0, 0, 0, 0, 0, 0, 0, 0]
 
 # PONER LA IP DE LA MÁQUINA VIRTUAL EN LA QUE ESTÉ CORRIENDO EL EMULADOR
-server = 'http://192.168.52.103:8000/'
+server = 'http://127.0.0.1:8000/'
 # server = 'http://10.0.21.132:8000/'
 http = urllib3.PoolManager()
 
