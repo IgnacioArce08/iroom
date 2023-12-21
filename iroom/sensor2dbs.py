33accfee641e30b72aafd84cc9ee01bfd654a13e
IgnacioArce08 <ignacio.arce@edu.uah.es>
2023-12-21 14:30:53 +0100

    a

--- iroom/sensor2dbs.py
+++ iroom/sensor2dbs.py
@@ -14,24 +14,25 @@ type_sensor = ['temperature', 'humidity', 'light', 'sound', 'motion']
 last_value = [0,0,0,0,0,0,0,0]
 
 #PONER LA IP DE LA MÁQUINA VIRTUAL EN LA QUE ESTÉ CORRIENDO EL EMULADOR
-server = 'http://192.168.56.103:8000/'
+server = 'http://127.0.0.1:8000/'
 http = urllib3.PoolManager()
 
 def updateSensor(code):
     value = 0
     try:
-        response = http.request('GET', server + type_sensor[code])
-        data = json.loads(response.data)
-        value = data[type_sensor[code]]
+            response = http.request('GET', f'{server}{type_sensor[code]}')
+            data = json.loads(response.data)
+            value = data[type_sensor[code]]
 
     except ValueError:
         print('Error de leer dato de sensor')
 
     if value != last_value[code]:
         try:  
-            # """ PARTE 1: COMPLETAR AQUÍ EL CÓDIGO PARA ESCRIBIR EN LA BASE DE DATOS EL VALOR DEL SENSOR"""
             print('Insertando...', type_sensor[code], '\t', value)
             cursor.execute("""INSERT INTO sensors(nombre, valor) values(%s, %s)""", (type_sensor[code], value))
+            last_value[code] = value
+            db.commit() 
         except ValueError:
             print('Error al insertar en base de datos')
         db.commit()
@@ -39,8 +40,7 @@ def updateSensor(code):
 
 def controlLightColor():
     try:
-        cursor.execute(
-            """SELECT valor FROM sensors WHERE nombre='red' order by time desc""")
+        cursor.execute("""SELECT valor FROM sensors WHERE nombre='red' order by time desc""")
         red = int(cursor.fetchone()[0])
         if (red != last_value[5]):
             last_value[5] = red
@@ -50,8 +50,7 @@ def controlLightColor():
         print('Error al consultar base de datos o conectar con iroom')
 
     try:
-        cursor.execute(
-            """SELECT valor FROM sensors WHERE nombre='green' order by time desc""")
+        cursor.execute("""SELECT valor FROM sensors WHERE nombre='green' order by time desc""")
         green = int(cursor.fetchone()[0])
         if (green != last_value[6]):
             last_value[6] = green
@@ -61,8 +60,7 @@ def controlLightColor():
         print('Error al consultar base de datos o conectar con iroom')
 
     try:
-        cursor.execute(
-            """SELECT valor FROM sensors WHERE nombre='blue' order by time desc""")
+        cursor.execute("""SELECT valor FROM sensors WHERE nombre='blue' order by time desc""")
         blue = int(cursor.fetchone()[0])
         if (blue != last_value[7]):
             last_value[7] = blue
@@ -74,24 +72,15 @@ def controlLightColor():
 if __name__ == "__main__":
     cursor = db.cursor(buffered=True)
     cursor.execute("""DROP table sensors""")
-    cursor.execute(
-        """create table sensors( time TIMESTAMP DEFAULT CURRENT_TIMESTAMP, nombre VARCHAR(15), valor INTEGER)""")
-    cursor.execute(
-        """INSERT INTO sensors(nombre, valor) values(%s, %s)""", ('temperature', 20))
-    cursor.execute(
-        """INSERT INTO sensors(nombre, valor) values(%s, %s)""", ('humidity', 40))
-    cursor.execute(
-        """INSERT INTO sensors(nombre, valor) values(%s, %s)""", ('light', 30))
-    cursor.execute(
-        """INSERT INTO sensors(nombre, valor) values(%s, %s)""", ('sound', 10))
-    cursor.execute(
-        """INSERT INTO sensors(nombre, valor) values(%s, %s)""", ('motion', 0))
-    cursor.execute(
-        """INSERT INTO sensors(nombre, valor) values(%s, %s)""", ('red', 20))
-    cursor.execute(
-        """INSERT INTO sensors(nombre, valor) values(%s, %s)""", ('blue', 20))
-    cursor.execute(
-        """INSERT INTO sensors(nombre, valor) values(%s, %s)""", ('green', 20))
+    cursor.execute("""create table sensors( time TIMESTAMP DEFAULT CURRENT_TIMESTAMP, nombre VARCHAR(15), valor INTEGER)""")
+    cursor.execute("""INSERT INTO sensors(nombre, valor) values(%s, %s)""", ('temperature', 20))
+    cursor.execute("""INSERT INTO sensors(nombre, valor) values(%s, %s)""", ('humidity', 40))
+    cursor.execute("""INSERT INTO sensors(nombre, valor) values(%s, %s)""", ('light', 30))
+    cursor.execute("""INSERT INTO sensors(nombre, valor) values(%s, %s)""", ('sound', 10))
+    cursor.execute("""INSERT INTO sensors(nombre, valor) values(%s, %s)""", ('motion', 0))
+    cursor.execute("""INSERT INTO sensors(nombre, valor) values(%s, %s)""", ('red', 20))
+    cursor.execute("""INSERT INTO sensors(nombre, valor) values(%s, %s)""", ('blue', 20))
+    cursor.execute("""INSERT INTO sensors(nombre, valor) values(%s, %s)""", ('green', 20))
     db.commit()
     print('Base de datos creada, comienza la consulta de sensores')
     while True:
