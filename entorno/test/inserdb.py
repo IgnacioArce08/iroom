9d4827a5fda908204551351a00a66d65217e3899
IgnacioArce08 <ignacio.arce@edu.uah.es>
2023-12-21 13:55:48 +0100

    a

--- entorno/test/inserdb.py
+++ entorno/test/inserdb.py
@@ -4,15 +4,22 @@
 import sys
 import mysql.connector
 
-db = mysql.connector.connect(host = "localhost", user = "nacho", passwd = "@Nacho08", db = "iroom")
-sensor = sys.argv[1]
-valor = sys.argv[2]
-cursor=db.cursor(buffered=True)
-try:
-	cursor.execute ("""INSERT INTO sensors(nombre, valor) values(%s, %s)""", (sensor, str(valor)))
-except:
-	print ('Error al escribir en la base de datos')
-db.commit()
-cursor.close()
-db.close()
+def insertar_en_base_de_datos(sensor, valor):
+    try:
+        with mysql.connector.connect(host="localhost", user="nacho", passwd="@Nacho08", db="iroom") as db:
+            with db.cursor(buffered=True) as cursor:
+                cursor.execute("INSERT INTO sensors(nombre, valor) VALUES (%s, %s)", (sensor, str(valor)))
+                db.commit()
+        print("Datos insertados correctamente.")
+    except mysql.connector.Error as err:
+        print(f"Error al escribir en la base de datos: {err}")
 
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Uso: python script.py <sensor> <valor>")
+        sys.exit(1)
+
+    sensor = sys.argv[1]
+    valor = sys.argv[2]
+
+    insertar_en_base_de_datos(sensor, valor)
